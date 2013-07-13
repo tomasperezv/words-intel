dc74f12787a4c924bba2ca80aed7bc5497b06f68
Tomas Perez <tom@0x101.com>
2013-07-13 22:02:15 +0400

    Extending the functionality of WordsIntel.

--- www/js/words-intel.js
+++ www/js/words-intel.js
@@ -30,3 +30,122 @@ var WordsIntel = function(text) {
 	];
 
 };
+
+/**
+ * @method isExcluded
+ * @param {String} word
+ * @return {Boolean}
+ * @public
+ */
+WordsIntel.prototype.isExcluded = function(word) {
+	return (word.length <= 3 || this._excluded.indexOf(word) > -1);
+};
+
+/**
+ * @method _applyFilters
+ * @private
+ */
+WordsIntel.prototype._applyFilters = function() {
+	for (var i = 0; i < this._filter.length; i++) {
+		var regex = new RegExp(this._filter[i], 'g');
+		this._text = this._text.replace(regex, ' ');
+	}
+};
+
+/**
+ * @param {Object} obj
+ * @method _getSortedKeys
+ * @return {Object}
+ * @private
+ */
+WordsIntel.prototype._getSortedKeys = function(obj) {
+
+	var keys = []; 
+	for(var key in obj) {
+		keys.push(key);
+	}
+
+	return keys.sort(function(a,b){
+		return obj[a]-obj[b]
+	});
+
+};
+
+/**
+ * @return {Array}
+ * @method getCloudsMapData
+ * @public
+ */
+WordsIntel.prototype.getCloudsMapData = function() {
+
+	// Generating data for the cloud map
+	var limit = 75,
+			current = 1,
+			data = [];
+
+	for (var i = (this.sortedWords.length - 1); i >= 0; i--) {
+
+		var value = (parseInt(this.stats[this.sortedWords[i]], 10)*2);
+		if (value > 200) {
+			value = 150;
+		}
+
+		if (value < 10) {
+			value = value * 10;
+		}
+
+		data.push({text: this.sortedWords[i], size: value});
+
+		current++;
+		if (current >= limit) {
+			break;
+		}
+
+	}
+
+	return data;
+
+};
+
+/**
+ * @method init
+ * @public
+ */
+WordsIntel.prototype.init = function() {
+
+	// Case insensitive
+	var content = this._text.toLowerCase();
+
+	console.log('Initial length: ' + content.length);
+	
+	this._applyFilters();
+	console.log('Size after filtering: ' + content.length);
+	
+	// Split by words
+	var words = content.split(/\s+/);
+	
+	// Gather information
+	this.stats = {};
+
+	console.log('Processing ' + words.length + ' words...');
+	
+	for (var i = 0; i < words.length; i++) {
+		var currentWord = words[i];
+		
+		if (!this.isExcluded(currentWord)) {
+			if (typeof this.stats[currentWord] === 'undefined') {
+				this.stats[currentWord] = 1;
+			} else {
+				this.stats[currentWord]++;
+			}
+		}
+	}
+	
+	this.sortedWords = this._getSortedKeys(this.stats);
+	
+	for (var i = (this.sortedWords.length - 1); i >= 0; i--) {
+		console.log((this.sortedWords.length - i) + "th '" + this.sortedWords[i] + "', " + this.stats[this.sortedWords[i]]);
+	}
+	
+
+};
